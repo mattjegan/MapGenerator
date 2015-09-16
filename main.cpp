afcb80ba53cf29e0ca122cf108bce41d0f159acb
tolo137 <matthewj.egan@hotmail.com>
2015-09-16 18:23:14 +1000

    Produces random noise between two non-negative values

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <vector>
 
 void generateMap(int n);
+void addNoise(std::vector<std::vector<int> > &mapRep);
+void digMap(std::vector<std::vector<int> > &mapRep);
+void displayMap(std::vector<std::vector<int> > &mapRep);
 
 int main(int argc, char **argv) {
 
@@ -22,12 +27,42 @@ void generateMap(int n) {
         *(it) = std::vector<int>(n);
     }
 
+    // Add noise
+    addNoise(mapRep);
+
+    // Dig through map
+    // TODO
+
     // Display map
+    displayMap(mapRep);
+
+}
+
+void addNoise(std::vector<std::vector<int> > &mapRep) {
+
+    unsigned int max = 5;
+    unsigned int min = 0;
+
+    std::srand(0); // For testing only
+    //std::srand(std::time(0));
+
     for (auto it = mapRep.begin(); it < mapRep.end(); ++it) {
         for (auto it2 = (*it).begin(); it2 < (*it).end(); ++it2) {
-            std::cout << *(it2);
+            *it2 = (std::rand() % (abs(max + 1) + abs(min)));
+        }
+    }
+}
+
+void digMap(std::vector<std::vector<int> > &mapRep) {
+    return;
+}
+
+
+void displayMap(std::vector<std::vector<int> > &mapRep) {
+    for (auto it = mapRep.begin(); it < mapRep.end(); ++it) {
+        for (auto it2 = (*it).begin(); it2 < (*it).end(); ++it2) {
+            std::cout << ' ' << *(it2);
         }
         std::cout << std::endl;
     }
-
 }
